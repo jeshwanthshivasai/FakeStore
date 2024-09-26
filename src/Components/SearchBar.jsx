d80b167f4bb415b7fbed4573a11f291335ac4d3d
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-09-26 15:03:31 +0530

    fetching and filtering done

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import '../Styles/SearchBar.css';
-import axios from 'axios';
 
 const SearchBar = () => {
   const [search, setSearch] = useState('');
   const [data, setData] = useState([]);
+
   useEffect(() => {
-    axios
-      .get('https://fakestoreapi.com/products')
-      .then((Response) => {
-        console.log(response);
-        setData(response.data);
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
       })
       .catch((error) => {
         console.log(error);
@@ -33,27 +32,26 @@ const SearchBar = () => {
       {
         data
           .filter((row) => {
-            if(search == '') {
+            if (search === '') {
               return row;
-            }
-            else if (
-              row.title.toLowerCase()
-              .includes(search.toLocaleLowerCase())) {
-                return row;
+            } else if (
+              row.title.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return row;
             }
           })
           .map((row, i) => {
             return (
-              <div className="card">
+              <div className="card" key={i}>
                 <div className="image">
-                  <img src={row.image} alt={row.image} />
+                  <img src={row.image} alt={row.title} />
                 </div>
                 <div className="title">
                   <h2>{row.title.substring(0, 20)}</h2>
                   <p>${row.price}</p>
                 </div>
               </div>
-            )
+            );
           })
       }
     </div>
